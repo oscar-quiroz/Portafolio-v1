6954f979c146417d02fbbf3f6d6f187d214a956d
oscar-quiroz <kairox.rt@gmail.com>
2021-01-15 12:45:07 -0500

    home terminado

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+
+  getImage(image: any, time: string) {
+    const t1 = '06:00';
+    const t2 = '18:00';
+    if (time >= t1 && time < t2) {
+      return ('/images/morning.png');
+    } else {
+      return ('/images/evening.png');
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
