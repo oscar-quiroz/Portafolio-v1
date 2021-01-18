a613e8b0dab285e02c2ae7c3fdab24b08bae601d
oscar-quiroz <kairox.rt@gmail.com>
2021-01-18 11:16:50 -0500

    media querys para componente skilList

--- frontend/src/app/components/skil-list/skil-list.component.ts
+++ frontend/src/app/components/skil-list/skil-list.component.ts
@@ -37,15 +37,15 @@ export class SkilListComponent implements OnInit {
       "desc": "BOOTSTRAP"
     } ,
     {
-      "img": "https://www.flaticon.es/svg/vstatic/svg/919/919827.svg?token=exp=1610757727~hmac=9d2f1b6c36e6d4490f2dd2054113246e",
-      "desc": "HTML"
+      "img": "http://www.xaviro.com/assets/img/skills/angular.svg",
+      "desc": "ANGULAR"
     },
     {
-      "img": "https://www.flaticon.es/svg/vstatic/svg/919/919826.svg?token=exp=1610758067~hmac=0afb544911430cfb90c83c41d67e4fe6",
-      "desc": "CSS"
+      "img": "http://www.xaviro.com/assets/img/skills/angular.svg",
+      "desc": "ANGULAR"
     },{
-      "img": "https://www.flaticon.es/svg/vstatic/svg/541/541509.svg?token=exp=1610758688~hmac=6a09634a37bf01f044b33dff36b7538b",
-      "desc": "javascript"
+      "img": "http://www.xaviro.com/assets/img/skills/angular.svg",
+      "desc": "ANGULAR"
     }
    ]
   
