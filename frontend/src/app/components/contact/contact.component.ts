5981f32e18424c756fdb7ce0efac8b6d95bea99e
oscar-quiroz <kairox.rt@gmail.com>
2021-01-18 19:48:29 -0500

    consimiendo API de mensajes

--- frontend/src/app/components/contact/contact.component.ts
+++ frontend/src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+ 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public sendEmail(e: Event) {
+    alert("entró")
+    console.log("entró al evento")
+    e.preventDefault();
+    emailjs.sendForm('service_jjv06sh', 'template_f6hdl17', e.target as HTMLFormElement, 'user_HQLJhDumGGDaDB5JrWBpR')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+        console.log(result, "result solo");
+                console.log("mensaje enviado");
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+
 
 }
